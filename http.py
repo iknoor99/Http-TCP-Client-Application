48c9f294d5451284344df066b03d1dc10ba7df07
iknoor99 <iknoor99@hotmail.com>
2019-09-22 01:14:14 -0400

    http post

--- http.py
+++ http.py
@@ -21,7 +21,24 @@ class http:
         client.connect((host,port))
 
         request = "POST / HTTP/1.1\r\nHost:%s\r\n\r\n" % host
-        client.send(request.encode("utf:8"))
+
+        headers = """\
+        POST /auth HTTP/1.1\r
+        Content-Type: {content_type}\r
+        Content-Length: {content_length}\r
+        Host: {host}\r
+        Connection: close\r
+        \r\n"""
+
+        header_bytes = headers.format(
+            content_type=" text/html",
+            content_length=3000,
+            host=str(host) + ":" + str(port)
+        )
+
+        request1=request+header_bytes
+
+        client.send(request1.encode("utf:8"))
 
         response = client.recv(4096)
         response = response.decode("utf:8")
