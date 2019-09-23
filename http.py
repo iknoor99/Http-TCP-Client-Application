56f597a6f12d19d790cc3c6b64d3b1b387cfb029
iknoor99 <iknoor99@hotmail.com>
2019-09-22 20:25:28 -0400

    http post

--- http.py
+++ http.py
@@ -15,30 +15,22 @@ class http:
 
         print(response)
 
-    def post(selfself,host,port):
+    def post(self,host,port):
 
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((host,port))
 
         request = "POST / HTTP/1.1\r\nHost:%s\r\n\r\n" % host
 
-        headers = """\
-        POST /auth HTTP/1.1\r
-        Content-Type: {content_type}\r
-        Content-Length: {content_length}\r
-        Host: {host}\r
-        Connection: close\r
-        \r\n"""
+        #message = "POST /auth HTTP/1.1\r\n"
+        #parameters = "userName=Ganesh&password=pass\r\n"
 
-        header_bytes = headers.format(
-            content_type=" text/html",
-            content_length=3000,
-            host=str(host) + ":" + str(port)
-        )
+        contentLength = "Content-Length: " + str(len(request))
+        contentType = "Content-Type: application/x-www-form-urlencoded\r\n"
 
-        request1=request+header_bytes
+        finalMessage = request + contentLength + contentType + "\r\n"
 
-        client.send(request1.encode("utf:8"))
+        client.send(finalMessage.encode("utf:8"))
 
         response = client.recv(4096)
         response = response.decode("utf:8")
@@ -46,6 +38,7 @@ class http:
         print(response)
 
 http().post("www.google.com",80)
+
 
 
 
