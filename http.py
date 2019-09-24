807d396800062ec73ccf43c0852ca65eac97cc93
Harsh Deep Kour <kourharsh@gmail.com>
2019-09-24 00:00:54 -0400

    Http get and post implemented

--- http.py
+++ http.py
@@ -1,48 +1,93 @@
 import socket
+from urllib.parse import urlparse
 
-class http:
 
-    def get(self,host,port):
+class Http:
+    def __init__(self, url, body, headers):  # constructor to add values
+        self.port = 80
+        self.host = urlparse(url).netloc
+        self.body = body
+        self.header = headers
 
+    def display_msg(self, msg):
+        header_l = msg.split('\r\n\r\n')
+        str_upper = header_l[0]
+        str_lines = str_upper.split('\r\n')
+        first_line = str_lines[0].split(' ')
+        resp_code = first_line[1]
+        print("\nResponse code: ")
+        print(resp_code)
+        resp_msg = first_line[2]
+        print("\nResponse message: ")
+        print(resp_msg)
+        print("\nHeaders:")
+        for line in range(1,len(str_lines)):
+            print(str_lines[line])
+        print("\nBody:")
+        for body_line in range(1, len(header_l)):
+            print(header_l[body_line])
+
+
+    def get(self, request):
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((host,port))
-
-        request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % host
+        client.connect((self.host, self.port))
         client.send(request.encode("utf:8"))
-
         response = client.recv(4096)
         response = response.decode("utf:8")
+        print(str(response))
+        client.close()
+        self.display_msg(response)
 
-        print(response)
 
-    def post(self,host,port):
+    def post(self, request):
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((self.host, self.port))
+            client.send(request.encode("utf-8"))
+            response = client.recv(4096)
+            response = response.decode("utf-8")
+            print("Response: \n\n" + str(response))
+            self.display_msg(str(response))
+        except:
+            print("Error receiving response!")
+        finally:
+            client.close()
 
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((host,port))
 
-        request = "POST /post HTTP/1.1\r\nHost: %s" % host
+    def post_request(self):
+        request = "POST /post HTTP/1.1\r\n" + "Host: " + self.host+"\r\n" + self.header+"\r\n" + self.body
+        print(request)
+        self.post(request)
 
-        create_header = {'User-Agent': 'Concordia-HTTP/1.0',
-              'Content-Type': 'application/json',
-              'Content-Length': '14'
-              }
+    def get_request(self):
+        request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % self.host
+        self.get(request)
 
-        start = "\r\n"
 
-        for key,value in create_header.items():
-            start += key + ": " + value
-            start +="\r\n"
+def create_header(header,bodyvalue):  # to convert header dictionary to header_string
+    header_string = ""
+    if "Content-Type" in header:
+        pass
+    else:
+        header["Content-Type"] = "application/json"
+    if "Content-Length" in header:
+        pass
+    else:
+        header["Content-Length"] = str(len(bodyvalue))
 
-        payload = request+start+"\r\n{Assignment:1}"
+    for key, value in header.items():
+        header_string = header_string + key + ": " + value + "\r\n"
+    return header_string
 
-        client.send(payload.encode("utf-8"))
 
-        response = client.recv(4096)
-        response = response.decode("utf-8")
+data = {'User-Agent': 'Concordia-HTTP/1.0'}
+body = '{"Assignment":1}'
+header = create_header(data, body)
+Http('http://httpbin.org/post', body, header).post_request()
 
-        print(response)
 
-http().post("httpbin.org",80)
+
+
 
 
 
